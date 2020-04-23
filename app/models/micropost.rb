e54d6d2893203c2a98e3728746f3c6e8db762331
ice-crypto <testtest2tom@gmail.com>
2020-04-23 07:01:07 +0000

    favorite model と　user,micropostsのリレーション構築。 コンソールから確認ずみ。 残るは, c,v,rの実装

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites
+  has_many :likers, through: :favorites, source: :micropost
+  
   validates :content, presence: true, length: { maximum: 255 }
   
 end
